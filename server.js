bc8cf82f4f79e1167d5c7a9f354c8c1d77f091e8
Noahk <105943352+PixelKing118@users.noreply.github.com>
2024-11-06 15:22:10 -0500

    fresh

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 */
 
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const mongoose = require('mongoose');
 const Post = require('./public/js/Post');  // Import the Post model
