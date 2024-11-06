7387b13d4f8ea04b3910d6157f348fc7b3aec285
Noahk <105943352+PixelKing118@users.noreply.github.com>
2024-11-05 23:30:59 -0500

    fresh

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/forumDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 }).then(() => {
     console.log("Connected to MongoDB");
 }).catch(err => {
